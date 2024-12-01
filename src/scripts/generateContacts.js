ef67dbaa52ac1e875e677148f00c663a2f13a16d
Vitalii Artiukh <radial_car@yahoo.com>
2024-12-01 22:07:33 +0200

    finish

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,15 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const contactsList = await readContacts();
+    const newContacts = Array(number).fill(1).map(createFakeContact);
+    await writeContacts([...contactsList, ...newContacts]);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
 
 generateContacts(5);
